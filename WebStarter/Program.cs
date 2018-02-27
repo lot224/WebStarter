a401277c050a49514d4f165e866c96070dff4406
Bogus Hawtsauce <evad@lot224.net>
2018-02-27 12:07:43 -0500

    Added Config

--- WebStarter/Program.cs
+++ WebStarter/Program.cs
@@ -7,46 +7,63 @@ using System.Threading.Tasks;
 
 namespace WebStarter {
     class Program {
+
+        static int maxNameLength = 0;
+        static int maxUrlLength = 0;
+
         static void Main(string[] args) {
 
-            Task ccclx = Task.Factory.StartNew(() => ExecuteEndpoint("http://dev.api.ccclx/swagger/ui/index"));
-            Task client = Task.Factory.StartNew(() => ExecuteEndpoint("http://dev.api.client/swagger/ui/index"));
-            Task cms = Task.Factory.StartNew(() => ExecuteEndpoint("http://dev.api.cms/swagger/ui/index"));
-            Task imaging = Task.Factory.StartNew(() => ExecuteEndpoint("http://dev.api.imaging/swagger/ui/index"));
-            Task policy = Task.Factory.StartNew(() => ExecuteEndpoint("http://dev.api.policy/swagger/ui/index"));
-            Task safety = Task.Factory.StartNew(() => ExecuteEndpoint("http://dev.api.safety/swagger/ui/index"));
-            Task shared = Task.Factory.StartNew(() => ExecuteEndpoint("http://dev.api.shared/swagger/ui/index"));
-            Task sso = Task.Factory.StartNew(() => ExecuteEndpoint("http://dev.api.sso/swagger/ui/index"));
-            Task vendor = Task.Factory.StartNew(() => ExecuteEndpoint("http://dev.api.vendor/swagger/ui/index"));
+            List<ConfigEntry> Entries = new Config().Entries;
 
-            Task.WaitAll(ccclx, client, cms, imaging, policy, safety, shared, sso, vendor);
+            maxNameLength = (from x in Entries orderby x.Name.Length descending select x.Name.Length).FirstOrDefault();
+            maxUrlLength = (from x in Entries orderby x.Url.Length descending select x.Url.Length).FirstOrDefault();
 
-            Console.WriteLine(Environment.NewLine + "Completed, Everything should be awake now.");
+            List<Task> Tasks = new List<Task>();
+
+            foreach (ConfigEntry entry in Entries) {
+                if (entry.Active) {
+                    Task task = Task.Factory.StartNew(() => ExecuteEndpoint(entry));
+                    Tasks.Add(task);
+                }
+            }
+
+            Task.WaitAll(Tasks.ToArray());
+
+            Console.WriteLine(Environment.NewLine + "Completed, Everything should be awake now.  If not, run it again.");
             Console.WriteLine(Environment.NewLine + "Press any key to exit.");
             Console.ReadKey();
         }
 
+        static void ExecuteEndpoint(ConfigEntry entry, int attempt = 0) {
 
-        static void ExecuteEndpoint(string url, int retry = 0) {
+            attempt += 1;
 
-            switch (retry) {
-                case 0: Console.WriteLine("Executing " + url + " ( 1st attempt )"); break;
-                case 1: Console.WriteLine("Executing " + url + " ( 2nd attempt )"); break;
-                case 2: Console.WriteLine("Executing " + url + " ( 3rd attempt )"); break;
-                case 3: Console.WriteLine("Executing " + url + " ( final attempt )"); break;
+            string ordinal = "";
+
+            switch (attempt.ToString().Last()) {
+                case '1': ordinal = attempt + "st"; break;
+                case '2': ordinal = attempt + "nd"; break;
+                case '3': ordinal = attempt + "rd"; break;
+                default: ordinal = attempt + "th"; break;
             }
 
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            if (attempt == entry.MaxAttempts)
+                ordinal = "Final";
+
+            Console.WriteLine(string.Format("{0} {1} ({2} Attempt)", entry.Name.PadRight(maxNameLength), entry.Url.PadRight(maxUrlLength), ordinal));
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(entry.Url);
             request.AutomaticDecompression = DecompressionMethods.GZip;
 
             try {
                 request.GetResponse();
             } catch {
-                if (retry < 3) {
-                    System.Threading.Thread.Sleep(5000);
-                    ExecuteEndpoint(url, retry += 1);
+                if (attempt < entry.MaxAttempts) {
+                    System.Threading.Thread.Sleep(entry.TimeOut);
+                    ExecuteEndpoint(entry, attempt);
                 }
             }
         }
+
     }
 }
---
+++ WebStarter/config.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace WebStarter {
+    public class Config {
+
+        public List<ConfigEntry> Entries { get; set; }
+
+        public Config() {
+            Entries = LoadSolutions();
+
+            if (Entries.Count == 0) {
+                Entries.Add(new ConfigEntry() { Active = true, MaxAttempts = 1, Name = "Test 1", TimeOut = 5, Url = "http://127.0.0.1:80" });
+                Entries.Add(new ConfigEntry() { Active = true, MaxAttempts = 1, Name = "Test 2", TimeOut = 5, Url = "http://127.0.0.1:80" });
+                Entries.Add(new ConfigEntry() { Active = true, MaxAttempts = 1, Name = "Test 2", TimeOut = 5, Url = "http://127.0.0.1:80" });
+                Save(Entries);
+
+                Console.WriteLine("No config entries found, creating example entries.");
+                Console.WriteLine("Entries written to (" + FileName + ")." + Environment.NewLine);
+
+            }
+        }
+
+        private static void Save(object obj) {
+            string Data = Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+            File.WriteAllText(FileName, Data);
+        }
+
+        private static string FileName {
+            get {
+                var assembly = Assembly.GetExecutingAssembly();
+                var directory = new System.IO.FileInfo(assembly.Location).DirectoryName;
+                System.IO.Directory.CreateDirectory(directory);
+                return Path.Combine(directory, "config.json");
+            }
+        }
+
+        private static List<ConfigEntry> LoadSolutions() {
+            List<ConfigEntry> nResult = new List<ConfigEntry>();
+            if (System.IO.File.Exists(FileName)) {
+                string data = System.IO.File.ReadAllText(FileName);
+                nResult = Newtonsoft.Json.JsonConvert.DeserializeObject<List<ConfigEntry>>(data);
+            }
+            return nResult;
+        }
+    }
+
+
+    public class ConfigEntry {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public int TimeOut { get; set; }
+        public int MaxAttempts { get; set; }
+        public bool Active { get; set; }
+    }
+
+}
